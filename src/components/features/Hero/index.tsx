fe3f18ac9ae82b5c4cdedb31a4bf76ac1e32bb0f
JordanTate <JordanTateAdds@iCloud.com>
2025-05-12 21:57:08 +0100

    Increase Marquee iteration and add background gradient mask

--- src/components/features/Hero/index.tsx
+++ src/components/features/Hero/index.tsx
@@ -1,23 +1,35 @@
-import Marquee from "@/components/ui/Marquee"
-import styles from './hero.module.css'
+import Marquee from '@/components/ui/Marquee';
+import styles from './hero.module.css';
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.background}>
-        <Marquee speed={20}>
+        <Marquee speed={30}>
           <p>First Light Collective</p>
         </Marquee>
-        <Marquee speed={18} direction="right">
+        <Marquee speed={28} direction='right'>
           <p>First Light Collective</p>
         </Marquee>
-        <Marquee speed={16}>
+        <Marquee speed={26}>
           <p>First Light Collective</p>
         </Marquee>
-        <Marquee speed={14} direction="right">
+        <Marquee speed={24} direction='right'>
+          <p>First Light Collective</p>
+        </Marquee>
+        <Marquee speed={22}>
+          <p>First Light Collective</p>
+        </Marquee>
+        <Marquee speed={20} direction='right'>
+          <p>First Light Collective</p>
+        </Marquee>
+        <Marquee speed={18}>
+          <p>First Light Collective</p>
+        </Marquee>
+        <Marquee speed={16} direction='right'>
           <p>First Light Collective</p>
         </Marquee>
       </div>
     </section>
-  )
+  );
 }
