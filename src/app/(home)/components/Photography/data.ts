739d7fadf740e9c10ed924b2c9d17a77bf1efda0
JordanTate <JordanTateAdds@iCloud.com>
2026-06-09 08:16:18 +0100

    feat: add photography component

---
+++ src/app/(home)/components/Photography/data.ts
@@ -0,0 +1,38 @@
+import { GalleryImage } from '@/components/ui/Gallery/types';
+
+const galleryImages: GalleryImage[] = [
+  {
+    src: '/HOKA_SPEEDGOAT_DEMO.jpg',
+    alt: 'HOKA SPEEDGOAT DEMO',
+  },
+  {
+    src: '/PEOPLEONTOP.PNG',
+    alt: 'PEOPLE ON TOP',
+  },
+  {
+    src: '/IMG_6053.jpg',
+    alt: 'IMG 6053',
+  },
+  {
+    src: '/592D96D9-06FA-4853-A08F-C4E20D98C891.JPG',
+    alt: '592D96D9-06FA-4853-A08F-C4E20D98C891.JPG',
+  },
+  {
+    src: '/31E3F5AD-1B3E-46BB-BA46-C6392D552560.JPG',
+    alt: '31E3F5AD-1B3E-46BB-BA46-C6392D552560.JPG',
+  },
+  {
+    src: '/BF9255AF-A198-405E-BCE5-88B59DC784BC.JPG',
+    alt: 'BF9255AF-A198-405E-BCE5-88B59DC784BC.JPG',
+  },
+  {
+    src: '/08-06-2026,_18_56_41.jpg',
+    alt: '08-06-2026, 18:56:41',
+  },
+  {
+    src: '/08-06-2026,_18_56_40.jpg',
+    alt: '08-06-2026, 18:56:40',
+  },
+];
+
+export { galleryImages };
