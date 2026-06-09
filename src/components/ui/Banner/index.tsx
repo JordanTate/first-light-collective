eb157d22bdac05c195f6c98352387c2e2b261df8
JordanTate <JordanTateAdds@iCloud.com>
2026-06-09 08:20:21 +0100

    feat: add about page link

--- src/components/ui/Banner/index.tsx
+++ src/components/ui/Banner/index.tsx
@@ -1,11 +1,22 @@
 import Image from 'next/image';
 import logo from '../../../../public/logomark-black.png';
 import styles from './banner.module.css';
+import Link from 'next/link';
 
 export default function Banner() {
   return (
     <nav className={styles.nav}>
-      <Image src={logo} width={50} height={50} alt='Logo' />
+      <div></div>
+      <Link href={'/'}>
+        <Image src={logo} width={50} height={50} alt='Logo' />
+      </Link>
+      <div>
+        <Link href={'/about'}>
+          <span className='text-2xl uppercase font-bold text-black'>
+            About Me
+          </span>
+        </Link>
+      </div>
     </nav>
   );
 }
