f1fe1c3f35a45d66c1f4ea581cd58291f98ab6e6
Jordan Adds <Jordan@roeville.com>
2026-06-09 15:23:54 +0100

    feat: update social section to be mobile responsive

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             <h2 className='text-4xl uppercase font-bold'>Social</h2>
           </div>
         </hgroup>
-        <div className='grid grid-cols-2 grid-flow-row gap-4 py-16'>
+        <div className='grid lg:grid-cols-2 grid-flow-row gap-4 py-16 px-4'>
           <TikTokVideo url='https://www.tiktok.com/@angelnvmber/video/7629799113407630614' />
           <TikTokVideo url='https://www.tiktok.com/@angelnvmber/video/7510166998425275670' />
         </div>
